092f9938fc4c97d5c3f76ca451f645f180041360
cyberkillua <moshood988@gmail.com>
2023-09-14 15:48:25 +0100

    fix input isuue and added sepolia test net

--- src/components/Body.js
+++ src/components/Body.js
@@ -60,44 +60,63 @@ export default function Body() {
   const handleDonatebtn = async () => {
     setDonationAmount(inputValue);
     console.log(donationAmount);
-    await donate({
+
+    setInputValue("");
+  };
+
+  useEffect(() => {
+    console.log(donationAmount);
+    donate({
       // onComplete:
       // onError:
       onSuccess: handleSuccess,
       onError: (error) => console.log(error),
     });
-    setInputValue("");
-  };
+  }, [donationAmount]);
   return (
     <div className="p-5">
-      <p className="text-center text-xl py-2">Donate your coins</p>
-      <div className="pt-5">
-        <label for="amount">Amount you want to donate : </label>
-
-        <input
-          className="border-solid border-2 border-black"
-          type="number"
-          required
-          placeholder="Enter Amount"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4"
-          type="button"
-          onClick={handleDonatebtn}
-          disabled={isLoading || isFetching}
-        >
-          {isLoading || isFetching ? (
-            <div className="animate-spin spinner-border h-8 w-8 border-b-2 rounded-full"></div>
-          ) : (
-            "Donate"
-          )}
-        </button>
+      <p className="text-center text-lg text-red-500 font-semibold">
+        This is a play project. Please do not donate real money. You can use
+        SEPOLIA Testnet ETH for testing. Also minimum Donation is 0.1 SEPOLIA
+        Testnet ETH, sorry broke boys LMAO!!!
+      </p>
+      <h2 className="text-center text-xl font-semibold mt-4">
+        Donate Your Coins
+      </h2>
+      <div className="mt-6">
+        <label htmlFor="amount" className="block text-gray-700">
+          Amount you want to donate:
+        </label>
+        <div className="mt-2 relative rounded-md shadow-sm">
+          <input
+            id="amount"
+            className="form-input block w-full pr-10 sm:text-sm sm:leading-5 border-gray-300 rounded-md"
+            type="number"
+            required
+            placeholder="Enter Amount"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+            <span className="text-gray-500">SEPOLIA</span>
+          </div>
+        </div>
       </div>
-      <p className="py-5">
+      <button
+        className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50"
+        type="button"
+        onClick={handleDonatebtn}
+        disabled={isLoading || isFetching}
+      >
+        {isLoading || isFetching ? (
+          <div className="animate-spin h-8 w-8 border-b-2 rounded-full"></div>
+        ) : (
+          "Donate"
+        )}
+      </button>
+      <p className="mt-4 text-center">
         Total amount raised so far:{" "}
-        {ethers.utils.formatUnits(totalAmountDonated, "ether")} ETH{" "}
+        {ethers.utils.formatUnits(totalAmountDonated, "ether")} ETH
       </p>
     </div>
   );
